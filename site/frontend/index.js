27b0ec62e30bb46a58252f17fd175720dc8d1ee0
MARTINEZ CHRISTOPHE p1709105 <christophe.martinez@etu.univ-lyon1.fr>
2022-02-15 16:49:50 +0100

    Mise à jour de la carte

--- site/frontend/index.js
+++ site/frontend/index.js
@@ -2,7 +2,7 @@ var map = L.map('map').setView([45.784, 4.875], 15);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 4,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
